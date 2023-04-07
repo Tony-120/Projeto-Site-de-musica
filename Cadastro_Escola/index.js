3481838ddd0c8dce5a65d456b59609f7868a102f
Henrique Nascimento <henrique4562@hotmail.com>
2023-04-07 11:12:24 -0300

    cadastro Professor e Escola Primeros MicroServiços

--- Cadastro_Escola/index.js
+++ Cadastro_Escola/index.js
@@ -5,7 +5,7 @@ app.use(express.json())
 const escola = {};
 contador = 0;
 
-
+// get get
 app.get('/escola', (req, res) => {
     res.send(escola);
   });
--- Cadastro_Prof/index.js
+++ Cadastro_Prof/index.js
@@ -6,7 +6,7 @@ const ProfessorEscola = {};
 const ProfesorId = {};
 contador = 0;
 
-
+// para um porfesor se cadastrar em uma escola , mudar o uuidv4 pro nimero ibnterios sera o RA
   app.post('/escola/:id/professor',  (req, res) => {
     const idObs = uuidv4();
     const {cpf, nome, endereco, cep, telefone, email, especializacao } = req.body;
