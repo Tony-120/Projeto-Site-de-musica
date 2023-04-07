ddb05017188f9c70fac4c9bc6495f009da635252
Henrique Nascimento <henrique4562@hotmail.com>
2023-04-06 23:38:56 -0300

    Coloquei o cadastro da escola e professor

---
+++ Cadastro_Prof/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express()
+const { v4: uuidv4 } = require('uuid');
+app.use(express.json())
+const ProfessorEscola = {};
+const ProfesorId = {};
+contador = 0;
+
+
+  app.post('/escola/:id/professor',  (req, res) => {
+    const idObs = uuidv4();
+    const {cpf, nome, endereco, cep, telefone, email, especializacao } = req.body;
+    //req.params dá acesso à lista de parâmetros da URL
+    const ProfessorEscola =
+      ProfesorId[req.params.id] || [];
+      ProfessorEscola.push({ id: idObs, cpf, nome, endereco, cep, telefone, email, especializacao });
+      ProfesorId[req.params.id] =
+      ProfessorEscola;
+  
+    res.status(201).send(ProfessorEscola);
+  });
+
+
+app.get('/escola/:id/professor', (req, res) => {
+    res.send(ProfesorId[req.params.id] || []);
+});
+
+
+  app.listen(4000, () => {
+    console.log('Cadastro Professor. Porta 4000');
+  });
+
+  
+  
