6e94efabac17191acc8c136d15381f195dd4bd7a
HDNascimento0 <128192591+HDNascimento0@users.noreply.github.com>
2023-05-25 00:11:27 -0300

    Tela Home reponsiva , Faltou arrumar a foto Encontre sua Aula

--- projeto_front/src/compoent/Home.js
+++ projeto_front/src/compoent/Home.js
@@ -1,3 +1,4 @@
+
 import React, { useEffect } from "react";
 import PianoImg from './Imagens/piano.jpg'
 import Nota from './Imagens/icon music.PNG'
@@ -5,36 +6,41 @@ import EncontreAula from './Imagens/Encontre sua escola.PNG'
 import "./Home.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
 const Home = () => {
   useEffect(() => {
     // Trava a rolagem da página quando o componente é montado
     document.body.style.overflow = 'hidden';
   });
 
-
   return (
-    <>
-          <div class="m" id="conteudo">
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col">
+          <div className="m" id="conteudo">
             <strong className="texto">Aulas de Musica</strong> 
           </div>
-        <div class="position-relative">
-            <img src={EncontreAula} alt="EncontreAula"/>
+          <div className="position-relative">
+            <img className="mx-auto text-start" src={EncontreAula} alt="EncontreAula"/>
           </div>
-
-        <div class="text-right">
-          <img class="float-end mx-auto w-50 h-50" src={PianoImg} alt="piano"/>
         </div>
-            <div class="text-center">
-              <button class="btn btn-dark col-md-3 " type="submit" id="LeiaMais">Leia mais</button>
-            </div>
-          <div class= "rodape">
-          <img class="mt-5" src={Nota} alt="icon music"/>
-          </div>
-         
-   
-    </>
+        
+      </div>
+      <div className="row justify-content-center">
+        <div className="col-md-3 text-center ">
+          <button className="btn btn-dark ml-6  w-100" type="submit" id="LeiaMais">Leia mais</button>
+        </div>
+        
+      </div>
+      <div className="row mt-0 ml-6">
+        <div className="col text-start">
+        <div className="col text-end">
+          <img className="mx-auto text-start" src={PianoImg} alt="piano"/>
+        </div>
+          <img src={Nota} alt="icon music"/>
+        </div>
+      </div>
+    </div>
   )
 }
+
 export default Home
