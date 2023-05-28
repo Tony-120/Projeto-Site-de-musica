ec082f72422a0e6ac99e455bb6a40f9c6669edcb
Matheus <matheusmom100@gmail.com>
2023-05-28 19:29:59 -0300

    Tela Sobre

--- projeto_front/src/compoent/Sobre.js
+++ projeto_front/src/compoent/Sobre.js
@@ -1,27 +1,38 @@
 import React from 'react';
 import './Sobre.css';
+import Nota from './Imagens/icon music.PNG';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Sobre = () => {
   return (
-    <div className="sobre">
-      <h1>Escola de Música</h1>
+    <div class="">
+    <h1 class="mt-5 text-center">Projeto SONGG</h1>
+    <section class="hero">
       <p>
-        Bem-vindo à nossa Escola de Música, onde a paixão pela música encontra o ensino de alta qualidade.
-        Nossa missão é fornecer um ambiente inspirador e educacional para estudantes de todas as idades e níveis de habilidade.
-        Nossos professores altamente qualificados estão comprometidos em ajudar você a desenvolver suas habilidades musicais,
-        seja você um iniciante entusiasmado ou um músico avançado em busca de aprimoramento.
+        Bem-vindo ao Projeto SONGG, uma plataforma inovadora de cadastro de escolas de música. Nosso objetivo é conectar escolas de música de alta qualidade com alunos apaixonados pela arte da música.
       </p>
       <p>
-        Oferecemos uma ampla variedade de cursos e disciplinas musicais, desde aulas de instrumentos como piano, violão, bateria e violino,
-        até teoria musical, composição e prática de conjunto. Nossa abordagem de ensino combina elementos teóricos e práticos,
-        garantindo que você tenha uma base sólida de conhecimento musical e a capacidade de aplicá-lo de forma criativa.
+        O Projeto SONGG foi desenvolvido para simplificar o processo de busca e cadastro de escolas de música. Escolas interessadas em oferecer seus serviços podem se cadastrar facilmente em nossa plataforma, fornecendo informações detalhadas sobre suas especialidades, professores qualificados e instalações modernas.
       </p>
       <p>
-        Além das aulas regulares, também organizamos workshops, recitais e eventos especiais, nos quais você terá a oportunidade de
-        se apresentar e compartilhar sua paixão pela música com outros estudantes e membros da comunidade musical.
-        Estamos comprometidos em criar um ambiente acolhedor e encorajador, onde você possa se expressar e crescer como músico.
+        Além disso, os alunos têm a oportunidade de se cadastrar no Projeto SONGG, criando um perfil personalizado que reflete seus interesses musicais, nível de habilidade e localização geográfica. Com base nessas informações, nossa plataforma oferece uma função de busca que permite aos alunos encontrar as escolas mais próximas de sua região.
       </p>
+      <p>
+        Uma das principais características do Projeto SONGG é a exibição das escolas em um mapa interativo. Ao realizar uma busca, os alunos podem visualizar as escolas em um mapa e obter informações relevantes sobre cada uma delas. Essa funcionalidade torna o processo de escolha mais intuitivo e ajuda os alunos a tomar decisões informadas sobre onde buscar educação musical.
+      </p>
+      <p>
+        Valorizamos a qualidade do ensino musical e acreditamos que todos os estudantes merecem ter acesso a escolas de música que atendam às suas necessidades e expectativas. É por isso que nos empenhamos em criar uma plataforma que facilite essa conexão entre escolas e alunos, promovendo a troca de conhecimento e a paixão pela música.
+      </p>
+      <p>
+        O Projeto SONGG está comprometido em fornecer uma experiência de alta qualidade tanto para as escolas quanto para os alunos. Nossa equipe está sempre empenhada em aprimorar a plataforma, adicionando recursos e funcionalidades que beneficiem todos os usuários.
+      </p>
+      <p>
+        Junte-se ao Projeto SONGG e faça parte de uma comunidade vibrante de amantes da música. Cadastre-se agora mesmo e comece sua jornada musical conosco!
+      </p>
+      <img className="mt-4 w-25 h-25" src={Nota} alt="icon music" />
+      </section>
     </div>
+    
   );
 }
 
